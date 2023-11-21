a11a4eebc1c0643c5fbcfa4ddc21b67ede65ec15
nchp7 <133721544+nchp7@users.noreply.github.com>
2023-11-21 17:10:47 -0300

    me falta agregar el tema del funcionamiento de las vidas, pero despues vengo joya

--- script.js
+++ script.js
@@ -22,10 +22,26 @@ function playRound(playerSelection, computerSelection) {
 function updateScores(result) {
     if (result.includes("WIN")) {
         playerScore++;
+        computerLives--;
     } else if (result.includes("Lose")) {
         computerScore++;
+        playerLives--;
     }
+
+    const playerScoreElement = document.querySelector('.playerScoreCounter');
+    const computerScoreElement = document.querySelector('.computerScoreCounter');
+
+    playerScoreElement.textContent = `Player Score: ${playerScore}`;
+    computerScoreElement.textContent = `Computer Score: ${computerScore}`;
+
+    // const playerLivesElement = document.querySelector('.playerLivesCounter');
+    // const ComputerLivesElement = document.querySelector('.computerLivesCounter');
+
+    // playerLivesElement.textContent = `Player Lives: ${playerLives}`;
+    // ComputerLivesElement.textContent = `Computer Lives: ${computerLives}`;
+
     console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+    console.log(`Player Lives: ${playerLives}, Computer Lives: ${computerLives}`);
 }
 
 function game(playerSelection) {
@@ -33,16 +49,36 @@ function game(playerSelection) {
     const result = playRound(playerSelection, computerSelection);
     console.log(result);
     updateScores(result);
+    
+    const playerImagePath = `../rock-paper-scissors/images/${playerSelection}_img.jpg`;
+    playerImage.src = playerImagePath;
+    playerImage.hidden = false;
+    
+    const computerImagePath = `../rock-paper-scissors/images/${computerSelection}_img.jpg`;
+    computerImage.src = computerImagePath;
+    computerImage.hidden = false;
+
+    setTimeout(() => {
+        playerImage.hidden = true;
+        computerImage.hidden = true;
+    }, 750);
 }
 
 const buttons = document.querySelectorAll('button');
+const playerImage = document.getElementById("playerImg");
+const computerImage = document.getElementById("computerImg");
+
+var playerScore = 0;
+var computerScore = 0;
+
+var playerLives = 5;
+var computerLives = 5;
 
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
         const playerSelection = event.target.innerText.toLowerCase();
         game(playerSelection);
+        
     });
 });
 
-var playerScore = 0;
-var computerScore = 0;
