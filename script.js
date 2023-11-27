a731a79b89e54d2453cb5e1b02f8b0d89f648fb0
nchp7 <133721544+nchp7@users.noreply.github.com>
2023-11-27 09:20:34 -0300

    finished the game, i could make it nicer

--- script.js
+++ script.js
@@ -28,17 +28,11 @@ function updateScores(result) {
         playerLives--;
     }
 
-    const playerScoreElement = document.querySelector('.playerScoreCounter');
-    const computerScoreElement = document.querySelector('.computerScoreCounter');
-
     playerScoreElement.textContent = `Player Score: ${playerScore}`;
     computerScoreElement.textContent = `Computer Score: ${computerScore}`;
 
-    // const playerLivesElement = document.querySelector('.playerLivesCounter');
-    // const ComputerLivesElement = document.querySelector('.computerLivesCounter');
-
-    // playerLivesElement.textContent = `Player Lives: ${playerLives}`;
-    // ComputerLivesElement.textContent = `Computer Lives: ${computerLives}`;
+    playerLivesElement.textContent = `Player Lives: ${playerLives}`;
+    computerLivesElement.textContent = `Computer Lives: ${computerLives}`;
 
     console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
     console.log(`Player Lives: ${playerLives}, Computer Lives: ${computerLives}`);
@@ -61,8 +55,54 @@ function game(playerSelection) {
     setTimeout(() => {
         playerImage.hidden = true;
         computerImage.hidden = true;
-    }, 750);
+
+        if (playerLives === 0 ) {
+            resetGame();
+        } else if (computerLives === 0) {
+            resetGame();
+        }
+
+    }, 1000);
+
 }
+
+function resetGame() {
+    
+    playerImage.hidden = true;
+    computerImage.hidden = true;
+
+    if (playerScore === 5) {
+        winnerElement.textContent = 'Player WINS!';
+    } else if (computerScore === 5) {
+        winnerElement.textContent = 'Computer WINS!';
+    }
+
+    setTimeout(() => {
+        playerScore = 0;
+        computerScore = 0;
+        playerLives = 5;
+        computerLives = 5;
+
+        playerScoreElement.textContent = `Player Score: ${playerScore}`;
+        computerScoreElement.textContent = `Computer Score: ${computerScore}`;
+        playerLivesElement.textContent = `Player Lives: ${playerLives}`;
+        computerLivesElement.textContent = `Computer Lives: ${computerLives}`;
+
+        playerScoreElement.style.display = 'block';
+        computerScoreElement.style.display = 'block';
+        playerLivesElement.style.display = 'block';
+        computerLivesElement.style.display = 'block';
+
+        winnerElement.textContent = '';
+    }, 2000);
+}
+
+var playerScoreElement = document.querySelector('.playerScoreCounter');
+var computerScoreElement = document.querySelector('.computerScoreCounter');
+var playerLivesElement = document.querySelector('.playerLivesCounter');
+var computerLivesElement = document.querySelector('.computerLivesCounter');
+
+var winnerElement = document.querySelector('.winner-message');
 
 const buttons = document.querySelectorAll('button');
 const playerImage = document.getElementById("playerImg");
